a8dfb45ce0b9f13bdf4563bc30b432572e29663d
Brett C <brcannon@bellsouth.net>
2023-11-06 17:04:45 -0600

    seeded data

---
+++ db/seeds.sql
@@ -0,0 +1,33 @@
+
+
+
+INSERT INTO department (id, department_name)
+Values  
+(1, 'Grocery'),
+(2, 'Toys'),
+(3, 'Style'),
+(4, 'Electronics'),
+(5, 'Beauty'),
+(6, 'Essencials');
+
+SELECT * FROM department;
+
+INSERT INTO roles (id, title, salary, department_id)
+VALUES  
+(1, "Team Member", 50000, 1),
+(2, "Team Leader", 75000, 2),
+(3, "Executive Team Leader", 100000, 3);
+
+SELECT * FROM roles;
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES  
+( 1, 'Brett', 'Cannon', 1, 1 ),
+( 2, 'Justin', 'Hathaway', 1, 1 ),
+( 3, 'Anthony', 'Davis', 1, 2 ),
+( 4, 'Cole', 'Schmirbeck', 2, 3 ),
+( 5, 'Rich', 'Douglas', 3, 3 ),
+( 6, 'Zack', 'Smith', 1, 1 ),
+( 7, 'Shiloh', 'Ballard', 2, 3);
+
+SELECT * FROM employee;
